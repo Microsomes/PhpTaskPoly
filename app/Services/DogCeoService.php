f39240f335675b984de1eb23f17dd1156edc7103
Tayyab Javed <tayyab.javed@grv.media>
2023-05-30 21:45:11 +0100

    DogCeroService with the first test

---
+++ app/Services/DogCeoService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class DogCeoService
+{
+    public function getAllBreeds(): array
+    {
+        return [];
+    }
+}
---
+++ tests/Feature/DogCeoServiceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\DogCeoService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DogCeoServiceTest extends TestCase
+{
+
+    public DogCeoService $dogCeoService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->dogCeoService = new DogCeoService();
+    }
+
+   
+    /** @test */
+    public function it_returns_an_array(){
+        $this->assertIsArray($this->dogCeoService->getAllBreeds());
+    }
+
+}
