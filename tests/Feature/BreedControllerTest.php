8ae814ac95b43c0e324638e409d57a352a55ee57
Tayyab Javed <tayyab.javed@grv.media>
2023-05-30 22:25:58 +0100

    breed api and test

---
+++ tests/Feature/BreedControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BreedControllerTest extends TestCase
+{
+    
+    /** @test  */
+    public function it_has_breeds_route(){
+        $this->get('/api/breed')->assertStatus(200);
+    }
+
+    
+
+
+}
